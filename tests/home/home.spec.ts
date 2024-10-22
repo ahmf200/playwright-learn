dcdb003282690efdf373cec0a3903a9a62a12a59
Fahad Ahmed <fahad15@live.co.uk>
2024-10-22 11:30:26 +0100

    test: some clean up

--- tests/home/home.spec.ts
+++ tests/home/home.spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/login';
+import { LoginPage } from '../../pages/login';
 
 test('open page and login button is visible', async ({page}) => {
     const loginPage = new LoginPage(page);
     await loginPage.goToHomePage();
     await expect(loginPage.loginButton).toBeVisible();
-    // await page.goto('/');
-    // const loginButton = "login-button";
-    // await expect(page.getByTestId(loginButton)).toBeVisible();
 });
